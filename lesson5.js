b0c610534af7ed133389f751a93aed68731901c6
Oleh <dwaynwaid@gmail.com>
2023-11-06 21:41:38 +0100

    lesson5

---
+++ lesson5.js
@@ -0,0 +1,47 @@
+
+//exercise 1
+
+/*
+let name = prompt("What is your name: ?");
+let surname = prompt("What is your surname: ?");
+let age = Number(prompt("What is your age: ?"));
+
+if (age >= 18) {
+    console.log(name + ' ' + surname + ' has ' + age + ' years old');
+}
+else {
+    console.log("Your age is less than 18 years old!!!");
+}
+
+*/
+
+//exercise 2
+
+let first_count = Number(prompt("Your first count is: ?"));
+let second_count = Number(prompt("Your second count is: ?"));
+let symbol = prompt("Your symbol is: ?");
+
+if (symbol === '*') {
+    console.log("Result is " + first_count * second_count);
+}
+
+else if (symbol === '/') {
+    console.log("Result is " + first_count / second_count);
+}
+else if (symbol === '-') {
+    let minus = Number(first_count) - Number(second_count);
+    console.log("Result is " + minus);
+}
+
+else if (symbol === '+') {
+    let summ = Number(first_count) + Number(second_count);
+    console.log("Result is " + summ);
+}
+else {
+    console.log("Your symbol is not accepted!!!");
+}
+
+
+
+//exersice 3
+
