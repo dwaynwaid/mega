a2d1aecda42d13a5586758a43086c2c5fbbe6e03
Oleh Zemliak <dwaynwaid@gmail.com>
2023-11-19 20:57:59 +0100

    les8_1

--- lesson_8.js
+++ lesson_8.js
@@ -1,15 +1,19 @@
+
 const sum = (...args) => {
+    let suma = 0;
     for (let i = 0; i < args.length; i++) {
-        if (typeof args[i] === 'number') {
-            const rrr = sum[i];
-            console.log(rrr);
-        }
-        else {
+        if (typeof args[i] !== 'number') {
             console.log('All elements must be numbers!');
             return;
         }
+        else {
+
+            suma += args[i];
+        }
 
     }
+    console.log(suma);
 };
 
-sum(1);
+sum(1, 2, 5);
+sum('sdfsdfsdf sd fsd ');
