2416b7b7cf3bd703a7588f61a982533e7cd8b5ba
Oleh Zemliak <dwaynwaid@gmail.com>
2023-11-21 22:48:30 +0100

    les8_complete

--- lesson_8.js
+++ lesson_8.js
@@ -1,4 +1,4 @@
-
+/*
 const sum = (...args) => {
     let suma = 0;
     for (let i = 0; i < args.length; i++) {
@@ -17,3 +17,37 @@ const sum = (...args) => {
 
 sum(1, 2, 5);
 sum('sdfsdfsdf sd fsd ');
+
+*/
+
+
+/*
+
+const obj = {
+    id: 1,
+    name: "Tester Testowy",
+    pwdHash: 'dfdfsdfsdjflksdhgsdhgslgsfdmgvsngeioongdfgn',
+    isAdmin: true,
+    hasAvatar: true,
+};
+
+function filter(obj) {
+    const { id, name, hasAvatar } = obj;
+
+    const safeObj = {
+        id, name, hasAvatar
+    };
+
+    return safeObj;
+}
+
+console.log(filter(obj));
+
+*/
+
+const name = 'Smbd Test';
+const age = 35;
+const role = 'Admin';
+
+const info = `${name} has a role of  ${role} and is born in year' ${(new Date().getFullYear() - age)}`;
+console.log(info);
